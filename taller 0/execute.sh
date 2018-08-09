df828d7122005d32b940d1d5ec084fe8d3930268
Carlos Arturo <carlox216@utp.edu.co>
2018-08-09 15:57:23 -0500

    update excute sh file for ubuntu - taller matrices

---
+++ taller 0/execute.sh
@@ -0,0 +1,7 @@
+for ((i = 2; i <= $@; i++ )); do
+    python3 "generate input data.py" $i
+    for ((j = 1; j <= 10; j++ )); do
+        python3 "matrices-Secuencial.py"
+        python3 "matrices-Paralelo.py"
+    done
+done
